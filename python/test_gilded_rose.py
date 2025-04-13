99d5c1997ac2cde5507db33551f4e4f60708a65a
joe06031990 <joesenior07@gmail.com>
2025-04-13 15:02:54 +0100

    fixed testing

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -6,10 +6,17 @@ from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
     def test_foo(self):
-        items = [Item("foo", 0, 0)]
+        # Arrange: Create one item
+        items = [Item("foo", 10, 20)]
         gilded_rose = GildedRose(items)
+
+        # Act: Update quality
         gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+
+        # Assert: Name remains the same, quality and sell_in should be decreased by 1
+        self.assertEqual("foo", items[0].name)         # Name should not change
+        self.assertEqual(9, items[0].sell_in)          # sell_in decreases by 1
+        self.assertEqual(19, items[0].quality)         # quality decreases by 1
 
         
 if __name__ == '__main__':
